8f14bd3063224cd71683eb9830503da7335f9f24
yoskirukato <38753934+yoskirukato@users.noreply.github.com>
2023-06-07 22:49:21 +0300

    404

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 <div class="row content-page">
 	  <div class="col-md-9 content">
 			
-	  		<h1 style="text-align:center;">404 ОШИБКА!</h1>
+	  		<h1 style="text-align:center;">404 ОШИБКА!!!</h1>
 	  		<img src="http://printblog.ru/wp-content/themes/printTheme/img/404.png"></img>
 		  	<p><h4 style="text-align:center;">К сожалению, такой страницы нет. Возможно её никогда и небыло.</h4></p>
 			<p><h4 style="text-align:center;">Воспользуйтесь поиском по сайту чтобы найти интересующую вас информацию</h4></p>
