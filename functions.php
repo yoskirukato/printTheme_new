9a742aced288cd9f2ec370b787a1ac244ed9fd0e
yoskirukato <38753934+yoskirukato@users.noreply.github.com>
2024-07-26 11:12:42 +0300

    Update functions.php ins commet
    Тестовое описание внесения правок

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 //---------END-----------
 
 
-//Удаляем лишнее из head
+//Удаляем лишне е из head
 remove_action('wp_head', 'wp_generator');
 remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'rsd_link');
